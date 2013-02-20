e14e05451651a2264879a21b542a17156fcbb0ca
0noda <0n0da@mail.ru>
2013-02-21 01:23:58 +0400

    Первый комит!

---
+++ bidloExpl/bidloExpl/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace bidloExpl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string[] getDir(string path)
+        {
+            string[] dir = Directory.GetDirectories(path);
+            return dir;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) //НАЖАТИЕ НА ЭЛЕМЕНТ
+        {
+            string path = listBox1.SelectedItem.ToString();
+            string[] dirList = new string[20];
+            dirList = getDir(path);
+            for (int i = 0; i < dirList.Count(); i++)
+            {
+                listBox1.Items.Add(dirList[i]);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) //С:/
+        {
+            string path = @"C:\";
+            listBox1.Items.Add(path);
+        }
+
+        private void button2_Click(object sender, EventArgs e) //ОЧИСТКА
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
